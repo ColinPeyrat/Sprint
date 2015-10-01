5982ff42456a754c7c9d6b80481117e7ea3afed5
ColinPeyrat <colpain74@gmail.com>
2015-10-01 11:29:16 +0200

    Git ignore of db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// ToDo : Vérifier que la connexion est OK
-$db = new PDO("pgsql:host=localhost;dbname=m3104","ldama","hop");
-
-// Pour éviter d'avoir à réutiliser "global" par la suite
-function db() { global $db; return $db; }
-
