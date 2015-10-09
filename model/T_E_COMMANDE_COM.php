e532b2c0b0f5db97e37d94d480322aa61a8a5da3
GRUS <benji.grus@icloud.com>
2015-10-09 16:10:51 +0200

    Sell service can see all order, see the delivery address, see the client information, see all the product of the command

--- model/T_E_COMMANDE_COM.php
+++ model/T_E_COMMANDE_COM.php
@@ -7,4 +7,16 @@ class T_E_COMMANDE_COM extends Model
     protected $_T_E_ADRESSE_ADR;
     protected $_T_E_CLIENT_CLI;
     protected $_com_date;
+	
+	public function findByDate($date){
+        $c = $this->findAll();
+
+        $commande = array();
+        foreach($c as $k => $v){
+            if($v->com_date == $date){
+                array_push($commande,$v);
+            }
+        }
+        return $commande;
+    }
 }
