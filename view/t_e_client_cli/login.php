cd8c4631a5e2d461f887109135aa52f1e0eddc83
Valérian GRANDJEAN <valerian.grandjean@gmail.com>
2015-10-09 16:44:13 +0200

    fix some bugs

--- view/t_e_client_cli/login.php
+++ view/t_e_client_cli/login.php
@@ -1,11 +1,11 @@
 <h2>Connectez vous</h2>
 <form class="form-inline" method="post" action="?r=cli/login">
 	<div class="form-group col-xs-2">
-        <label class="form-label" for="login">E-mail</label>
+        <label class="form-label" for="login">E-mail :</label>
         <input class="form-control" type="text" name="login" id="login" />
     </div>
     <div class="form-group col-xs-2">
-        <label for="password">mot de passe</label>
+        <label for="password">Mot de passe :</label>
         <input class="form-control" type="password" name="password" id="password" />
     </div>
     <div class="form-group col-xs-3"><br/>
